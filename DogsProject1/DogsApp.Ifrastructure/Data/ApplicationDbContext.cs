1b37338aa9f3f723f5569e7e83524e11cd7184bb
vkachulev2022 <v.kachulev2022@pmg-pernik.com>
2026-05-11 08:14:26 +0300

    Dogs project 1 ot google driva mi
    Praveni v predishen chas

---
+++ DogsProject1/DogsApp.Ifrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DogsApp.Ifrastructure.Data.Domain;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DogsApp.Ifrastructure.Data 
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Dog> Dogs { get; set; }
+    }
+}
+
+
+
+
---
+++ DogsProject1/DogsApp.Ifrastructure/Data/Domain/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsApp.Ifrastructure.Data.Domain
+{
+    public class Dog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; } = null!;
+
+        [Range(0,30)]
+        public int Age { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Breed { get; set; } = null!;
+
+        public string? Picture {  get; set; }
+
+
+    }
+}
